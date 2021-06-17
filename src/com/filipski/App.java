40ffe47b9ad7de9aab8d76467377ed501ce284e3
dfilipski <danny.filipski@gmail.com>
2021-06-17 11:23:48 -0400

    Add slider and change listener

--- src/com/filipski/App.java
+++ src/com/filipski/App.java
@@ -1,14 +1,19 @@
 package com.filipski;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class App {
     private JButton buttonConvert;
     private JPanel panelMain;
     private JTextField celTemp;
     private JLabel farTemp;
+    private JSlider farSlider;
     private static ImageIcon img = new ImageIcon("Status-weather-clear-icon.png");
 
     public App() {
@@ -18,6 +23,13 @@ public class App {
                 double celTempVal = Double.parseDouble(celTemp.getText());
                 double farTempVal = celTempVal * 9/5 + 32;
                 farTemp.setText(String.valueOf(farTempVal));
+                farSlider.setValue((int) farTempVal);
+            }
+        });
+        farSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                farTemp.setText(String.valueOf(farSlider.getValue()));
             }
         });
     }
